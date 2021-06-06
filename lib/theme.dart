cd78281e94515e5f7fc25a50eef10e779b21c3bb
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2021-06-06 21:59:24 +0700

    Slicing Sign In Page

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+double defaultMargin = 30.0;
+
 Color primaryColor = Color(0XFF6C5ECF);
 Color secondaryColor = Color(0XFF38ABBE);
 Color alertColor = Color(0XFFED6363);
@@ -10,6 +12,7 @@ Color backgroundColor2 = Color(0XFF2B2937);
 Color backgroundColor3 = Color(0XFF242231);
 Color primaryTextColor = Color(0XFFF1F0F2);
 Color secondaryTextColor = Color(0XFF999999);
+Color subtitleColor = Color(0XFF504F5E);
 
 TextStyle primaryTextStyle = GoogleFonts.poppins(
   color: primaryTextColor,
@@ -23,8 +26,16 @@ TextStyle priceTextStyle = GoogleFonts.poppins(
   color: priceColor,
 );
 
+TextStyle subtitleTextStyle = GoogleFonts.poppins(
+  color: subtitleColor,
+);
+
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+  color: primaryColor,
+);
+
 FontWeight light = FontWeight.w300;
 FontWeight regular = FontWeight.w400;
 FontWeight medium = FontWeight.w500;
-FontWeight semoBold = FontWeight.w600;
+FontWeight semiBold = FontWeight.w600;
 FontWeight bold = FontWeight.w700;
