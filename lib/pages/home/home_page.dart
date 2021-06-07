2ae42b971483cd7f4db2532de55918c2537a7d5d
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2021-06-07 15:57:59 +0700

    Add Home Page Header

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -2,13 +2,58 @@ import 'package:flutter/material.dart';
 import 'package:shamo/theme.dart';
 
 class HomePage extends StatelessWidget {
+  Widget header() {
+    return Container(
+      margin: EdgeInsets.only(
+        top: defaultMargin,
+        left: defaultMargin,
+        right: defaultMargin,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Hallo, Alex',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 24,
+                    fontWeight: semiBold,
+                  ),
+                ),
+                Text(
+                  '@alexkeinn',
+                  style: subtitleTextStyle.copyWith(
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 54,
+            width: 54,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/image_profile.png',
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Home Page',
-        style: primaryTextStyle,
-      ),
+    return ListView(
+      children: [
+        header(),
+      ],
     );
   }
 }
