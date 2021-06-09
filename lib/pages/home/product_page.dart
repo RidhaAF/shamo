ad29e121293c8cb8687e5bdd81498d540ed74fbb
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2021-06-09 17:14:41 +0700

    Update Product Page Content

--- lib/pages/home/product_page.dart
+++ lib/pages/home/product_page.dart
@@ -275,6 +275,49 @@ class _ProductPageState extends State<ProductPage> {
                 ],
               ),
             ),
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.all(
+                defaultMargin,
+              ),
+              child: Row(
+                children: [
+                  Container(
+                    height: 54,
+                    width: 54,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                          'assets/button_chat.png',
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 54,
+                      child: TextButton(
+                        onPressed: () {},
+                        style: TextButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            backgroundColor: primaryColor),
+                        child: Text(
+                          'Add to Cart',
+                          style: primaryTextStyle.copyWith(
+                            fontWeight: semiBold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       );
