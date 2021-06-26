96b05a9eb34b59619d20a372493540b5bd506038
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2021-06-26 18:37:46 +0700

    Show Dynamic Bubble Chat

--- lib/pages/detail_chat_page.dart
+++ lib/pages/detail_chat_page.dart
@@ -204,6 +204,7 @@ class _DetailChatPageState extends State<DetailChatPage> {
                     .map((MessageModel message) => ChatBubble(
                           isSender: message.isFromUser,
                           text: message.message,
+                          product: message.product,
                         ))
                     .toList(),
               );
