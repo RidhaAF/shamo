1db75c55dc4e81abf7f82c241a62cfb9fcc71895
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2021-06-26 18:48:27 +0700

    Add Page Provider

--- lib/pages/home/main_page.dart
+++ lib/pages/home/main_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shamo/pages/home/chat_page.dart';
 import 'package:shamo/pages/home/home_page.dart';
 import 'package:shamo/pages/home/profile_page.dart';
 import 'package:shamo/pages/home/wishlist_page.dart';
+import 'package:shamo/providers/page_provider.dart';
 import 'package:shamo/theme.dart';
 
 class MainPage extends StatefulWidget {
@@ -15,6 +17,8 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
+    PageProvider pageProvider = Provider.of<PageProvider>(context);
+
     Widget cartButton() {
       return FloatingActionButton(
         onPressed: () {
@@ -42,12 +46,10 @@ class _MainPageState extends State<MainPage> {
           clipBehavior: Clip.antiAlias,
           child: BottomNavigationBar(
             backgroundColor: backgroundColor4,
-            currentIndex: currentIndex,
+            currentIndex: pageProvider.currentIndex,
             onTap: (value) {
               print(value);
-              setState(() {
-                currentIndex = value;
-              });
+              pageProvider.currentIndex = value;
             },
             type: BottomNavigationBarType.fixed,
             items: [
@@ -57,7 +59,9 @@ class _MainPageState extends State<MainPage> {
                   child: Image.asset(
                     'assets/icon_home.png',
                     width: 21,
-                    color: currentIndex == 0 ? primaryColor : Color(0XFF808191),
+                    color: pageProvider.currentIndex == 0
+                        ? primaryColor
+                        : Color(0XFF808191),
                   ),
                 ),
                 label: '',
@@ -68,7 +72,9 @@ class _MainPageState extends State<MainPage> {
                   child: Image.asset(
                     'assets/icon_chat.png',
                     width: 20,
-                    color: currentIndex == 1 ? primaryColor : Color(0XFF808191),
+                    color: pageProvider.currentIndex == 1
+                        ? primaryColor
+                        : Color(0XFF808191),
                   ),
                 ),
                 label: '',
@@ -79,7 +85,9 @@ class _MainPageState extends State<MainPage> {
                   child: Image.asset(
                     'assets/icon_wishlist.png',
                     width: 20,
-                    color: currentIndex == 2 ? primaryColor : Color(0XFF808191),
+                    color: pageProvider.currentIndex == 2
+                        ? primaryColor
+                        : Color(0XFF808191),
                   ),
                 ),
                 label: '',
@@ -90,7 +98,9 @@ class _MainPageState extends State<MainPage> {
                   child: Image.asset(
                     'assets/icon_profile.png',
                     width: 18,
-                    color: currentIndex == 3 ? primaryColor : Color(0XFF808191),
+                    color: pageProvider.currentIndex == 3
+                        ? primaryColor
+                        : Color(0XFF808191),
                   ),
                 ),
                 label: '',
@@ -102,7 +112,7 @@ class _MainPageState extends State<MainPage> {
     }
 
     Widget body() {
-      switch (currentIndex) {
+      switch (pageProvider.currentIndex) {
         case 0:
           return HomePage();
           // ignore: dead_code
@@ -125,7 +135,8 @@ class _MainPageState extends State<MainPage> {
     }
 
     return Scaffold(
-      backgroundColor: currentIndex == 0 ? backgroundColor1 : backgroundColor3,
+      backgroundColor:
+          pageProvider.currentIndex == 0 ? backgroundColor1 : backgroundColor3,
       floatingActionButton: cartButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: customBottomNav(),
